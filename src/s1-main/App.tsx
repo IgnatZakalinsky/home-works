e1642e5f2659839e372a6ac1fb55ee9364c45c0c
Andres <zadorozhnyi99.dev@gmail.com>
2022-06-21 12:10:49 +0200

    add dark theme support and some basic styles

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -5,20 +5,26 @@ import HW1 from '../s2-homeworks/hw01/HW1'
 // import HW3 from '../s2-homeworks/hw03/HW3'
 // import HW4 from '../s2-homeworks/hw04/HW4'
 import HW5 from '../s2-homeworks/hw05/HW5'
+import { useSelector } from 'react-redux'
+import { useEffect } from 'react'
 
 const App = () => {
-    return (
-        <div className={s.App}>
-            <div>react homeworks:</div>
+   const themeId = useSelector((state: any) => state.theme.themeId)
+   useEffect(() => {
+      document.documentElement.dataset.theme = themeId
+   }, [themeId])
+   return (
+      <div className={s.App} data-theme={themeId}>
+         <div>react homeworks:</div>
 
-            {/*<HW1/>*/}
-            {/*<HW2/>*/}
-            {/*<HW3/>*/}
-            {/*<HW4/>*/}
+         {/*<HW1/>*/}
+         {/*<HW2/>*/}
+         {/*<HW3/>*/}
+         {/*<HW4/>*/}
 
-            <HW5/>
-        </div>
-    )
+         <HW5 />
+      </div>
+   )
 }
 
 export default App
--- src/s2-homeworks/hw12/HW12.tsx
+++ src/s2-homeworks/hw12/HW12.tsx
@@ -2,43 +2,48 @@ import React from 'react'
 import s from './HW12.module.css'
 import s2 from '../../s1-main/App.module.css'
 import SuperSelect from '../hw07/common/c5-SuperSelect/SuperSelect'
-import {useDispatch, useSelector} from 'react-redux'
-import {changeThemeId} from './bll/themeReducer'
+import { useDispatch, useSelector } from 'react-redux'
+import { changeThemeId } from './bll/themeReducer'
 
 const themes = [
-    {id: 1, value: 'dark'},
-    {id: 2, value: 'red'},
-    {id: 3, value: 'some'},
+   { id: 1, value: 'light' },
+   { id: 2, value: 'dark' },
+   { id: 3, value: 'retro' },
 ]
 
 const HW12 = () => {
-    const themeId = useSelector((state: any) => state.theme.themeId)
-    const theme = themes.find(t => t.id === themeId)!.value
+   const themeId = useSelector((state: any) => state.theme.themeId)
+   const theme = themes.find((t) => t.id === themeId)!.value
 
-    const dispatch = useDispatch()
-    const change = (id: number) => {
-        dispatch(changeThemeId(id))
-    }
+   const dispatch = useDispatch()
+   const change = (id: number) => {
+      dispatch(changeThemeId(id))
+   }
 
-    return (
-        <div id={'hw12'} className={s2.hw + ' ' + s[theme]}>
-            <hr/>
-            {/*можно убрать этот тег*/}
+   return (
+      <div id={'hw12'} className={s2.hw + ' ' + s[theme]}>
+         <hr />
+         {/*можно убрать этот тег*/}
 
-            <div id={'hw12-text'} className={s2.hwTitle + ' ' + s[theme + '-text']}>homeworks 12</div>
-            <SuperSelect
-                id={'hw12-select-theme'}
-                value={themeId}
-                options={themes}
-                onChangeOption={change}
-            />
+         <div
+            id={'hw12-text'}
+            className={s2.hwTitle + ' ' + s[theme + '-text']}
+         >
+            homeworks 12
+         </div>
+         <SuperSelect
+            id={'hw12-select-theme'}
+            value={themeId}
+            options={themes}
+            onChangeOption={change}
+         />
 
-            <hr/>
-            {/*можно убрать этот тег*/}
-            <hr/>
-            {/*можно убрать этот тег*/}
-        </div>
-    )
+         <hr />
+         {/*можно убрать этот тег*/}
+         <hr />
+         {/*можно убрать этот тег*/}
+      </div>
+   )
 }
 
 export default HW12
