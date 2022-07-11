046d91d3300cbf8e4cbd3f5dd66decdbba740d51
Andres <andres99.dev@gmail.com>
2022-07-11 13:17:14 +0200

    hw5 add page name to header

--- src/s2-homeworks/hw05/header/Header.tsx
+++ src/s2-homeworks/hw05/header/Header.tsx
@@ -1,14 +1,25 @@
 import React, { FC } from 'react'
 import burgerIcon from './burger.svg'
 import s from './Header.module.css'
+import { useLocation } from 'react-router-dom'
+import { PATH } from '../Pages'
 
 type PropsType = {
-    open: boolean
     handleOpen: () => void
 }
 
-export const Header: FC<PropsType> = ({ open, handleOpen }) => {
+export const Header: FC<PropsType> = ({ handleOpen }) => {
     // hw5-menu изначально отсутствует, при нажатии на бургер - появляется, при повторном нажатии исчезает
+    const location = useLocation()
+    const currentPath = location.pathname
+    const pageName =
+        currentPath === PATH.PRE_JUNIOR
+            ? 'Pre Junior'
+            : currentPath === PATH.JUNIOR
+            ? 'Junior'
+            : currentPath === PATH.JUNIOR_PLUS
+            ? 'Junior Plus'
+            : 'Error'
     return (
         <>
             <div id={'hw5-header'} className={s.header}>
@@ -19,6 +30,7 @@ export const Header: FC<PropsType> = ({ open, handleOpen }) => {
                     onClick={handleOpen}
                     alt={'open menu'}
                 />
+                <h1>{pageName}</h1>
             </div>
         </>
     )
