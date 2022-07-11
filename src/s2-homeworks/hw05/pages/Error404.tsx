606319e8f9243f6c393813be409efd3c5f3aecae
Andres <andres99.dev@gmail.com>
2022-07-11 13:17:44 +0200

    hw5 add 404 svg

--- src/s2-homeworks/hw05/pages/Error404.tsx
+++ src/s2-homeworks/hw05/pages/Error404.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import s from './Error404.module.css'
-
+import error404 from './404.svg'
 const Error404 = () => {
     return (
         <div id={'hw5-page-404'}>
             <div className={s.wrapper}>
-                <span className={s.error404}>404</span>
+                <img src={error404} alt={'404'} className={s.error404} />
             </div>
         </div>
     )
