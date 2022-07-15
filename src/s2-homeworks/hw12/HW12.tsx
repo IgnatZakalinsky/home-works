418b6ce1d8f360bdef4c9ec9f7e03a6e94a907df
Andres <andres99.dev@gmail.com>
2022-07-15 11:17:45 +0200

    hw12 styles

--- src/s2-homeworks/hw12/HW12.tsx
+++ src/s2-homeworks/hw12/HW12.tsx
@@ -7,8 +7,8 @@ import { changeThemeId } from './bll/themeReducer'
 
 const themes = [
     { id: 1, value: 'light' },
-    { id: 2, value: 'dark' },
-    { id: 3, value: 'retro' },
+    { id: 2, value: 'blue' },
+    { id: 3, value: 'dark' },
 ]
 
 const HW12 = () => {
@@ -21,19 +21,18 @@ const HW12 = () => {
     }
 
     return (
-        <div id={'hw12'} className={s2.hw + ' ' + s[theme]}>
-            <div
-                id={'hw12-text'}
-                className={s2.hwTitle + ' ' + s[theme + '-text']}
-            >
-                homeworks 12
+        <div id={'hw12'}>
+            <div id={'hw12-text'} className={s2.hwTitle}>
+                Homework #12
             </div>
-            <SuperSelect
-                id={'hw12-select-theme'}
-                value={themeId}
-                options={themes}
-                onChangeOption={change}
-            />
+            <div className={s2.hw}>
+                <SuperSelect
+                    id={'hw12-select-theme'}
+                    value={themeId}
+                    options={themes}
+                    onChangeOption={change}
+                />
+            </div>
         </div>
     )
 }
