816a387f72932dbf688c2721194cbd9942c57cf5
neko <ai73a@yandex.ru>
2022-09-14 14:17:08 +0300

    final ref hw2 / with tasks

--- src/s2-homeworks/hw02/HW2.tsx
+++ src/s2-homeworks/hw02/HW2.tsx
@@ -1,6 +1,20 @@
-import React, { useState } from 'react'
-import Affairs from './Affairs'
+import React, {useState} from 'react'
+import Affairs from './affairs/Affairs'
 import s2 from '../../s1-main/App.module.css'
+
+/*
+* 1 - описать типы AffairPriorityType, AffairType
+* 2 - указать нужный тип для defaultAffairs
+* 3 - дописать типы и логику функции filterAffairs и проверить её тестами
+* 4 - выполнить пункт 3 для функции deleteAffair
+* 5 - указать нужный тип в useState с affairs
+* 6 - дописать тип и логику функции deleteAffairCallback
+* 7 - в файле Affairs.tsx дописать типизацию пропсов
+* 8 - в файле Affairs.tsx дописать логику функций setAll, setHigh, setMiddle, setLow
+* 9 - в файле Affair.tsx дописать типизацию пропсов
+* 10 - в файле Affair.tsx дописать функции deleteCallback и использовать
+* 11 - в файле Affair.tsx отобразить приходящие данные
+* */
 
 // types
 export type AffairPriorityType = any // need to fix any
@@ -13,11 +27,11 @@ export type FilterType = 'all' | AffairPriorityType
 
 // constants
 const defaultAffairs: any = [ // need to fix any
-    { _id: 1, name: 'React', priority: 'high' }, // студенты могут изменить содержимое name и количество элементов в массиве, ...priority не менять!
-    { _id: 2, name: 'anime', priority: 'low' },
-    { _id: 3, name: 'games', priority: 'low' },
-    { _id: 4, name: 'work', priority: 'high' },
-    { _id: 5, name: 'html & css', priority: 'middle' },
+    {_id: 1, name: 'React', priority: 'high'}, // студенты могут изменить содержимое name и количество элементов в массиве, ...priority не менять!
+    {_id: 2, name: 'anime', priority: 'low'},
+    {_id: 3, name: 'games', priority: 'low'},
+    {_id: 4, name: 'work', priority: 'high'},
+    {_id: 5, name: 'html & css', priority: 'middle'},
 ]
 
 // pure helper functions
@@ -36,8 +50,9 @@ function HW2() {
     const [filter, setFilter] = useState<FilterType>('all')
 
     const filteredAffairs = filterAffairs(affairs, filter)
-    const deleteAffairCallback = (_id: any) => // need to fix any
-        setAffairs(deleteAffair(affairs, _id))
+    const deleteAffairCallback = (_id: any) => { // need to fix any
+        setAffairs(deleteAffair(affairs, _id)) // need to fix
+    }
 
     return (
         <div id={'hw2'}>
