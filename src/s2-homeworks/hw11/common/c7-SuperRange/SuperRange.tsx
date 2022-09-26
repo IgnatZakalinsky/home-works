2a8af4599e418b25076237c8a81f92f1bd950fa8
neko <ai73a@yandex.ru>
2022-09-26 10:28:48 +0300

    final ref hw11

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -1,44 +1,35 @@
 import React from 'react'
-import { Slider, SliderProps } from '@mui/material'
+import {Slider, SliderProps} from '@mui/material'
 
-// здесь мы говорим что у нашего слайдера будут такие же пропсы как у обычного mui слайдера
-// (чтоб не писать value: string, onChange: ...; они уже все описаны в DefaultInputPropsType)
-type SuperRangePropsType = SliderProps
-
-const SuperRange: React.FC<SuperRangePropsType> = ({
-    ...restProps // все остальные пропсы попадут в объект restProps
-}) => {
+const SuperRange: React.FC<SliderProps> = (props) => {
     return (
-        <>
-            <Slider
-                id={'hw11-single-slider'}
-                sx={{
-                    width: '160px',
-                    color: '#00CC22',
-                    '& .MuiSlider-track': {
-                        border: 'none',
+        <Slider
+            sx={{ // стили для слайдера // пишет студент
+                width: '160px',
+                color: '#00CC22',
+                '& .MuiSlider-track': {
+                    border: 'none',
+                },
+                '& .MuiSlider-thumb': {
+                    width: 20,
+                    height: 20,
+                    border: '1px solid #00CC22',
+                    backgroundColor: '#fff',
+                    '&:before': {
+                        content: '""',
+                        width: '6px',
+                        height: '6px',
+                        borderRadius: '50%',
+                        backgroundColor: '#00CC22',
+                        zIndex: -1,
                     },
-                    '& .MuiSlider-thumb': {
-                        width: 20,
-                        height: 20,
-                        border: '1px solid #00CC22',
-                        backgroundColor: '#fff',
-                        '&:before': {
-                            content: '""',
-                            width: '6px',
-                            height: '6px',
-                            borderRadius: '50%',
-                            backgroundColor: '#00CC22',
-                            zIndex: -1,
-                        },
-                        '&:hover, &.Mui-focusVisible, &.Mui-active': {
-                            boxShadow: 'none',
-                        },
+                    '&:hover, &.Mui-focusVisible, &.Mui-active': {
+                        boxShadow: 'none',
                     },
-                }}
-                {...restProps} // отдаём инпуту остальные пропсы если они есть (value например там внутри)
-            />
-        </>
+                },
+            }}
+            {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
+        />
     )
 }
 
